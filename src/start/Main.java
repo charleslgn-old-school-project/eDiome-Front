a2be163ff4767d0f8b0cc718c378fb159ea81b4a
CharlesLigo <charles.ligony3@etu.univ-lorraine.fr>
2018-10-21 16:48:47 +0200

    ghjk

--- src/start/Main.java
+++ src/start/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../gui/sample.fxml"));
         primaryStage.setTitle("IRC");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
