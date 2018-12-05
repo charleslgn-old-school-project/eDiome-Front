d519ead6d6f5d815fc4d186f82716afd87598803
charles ligony <c-ligony@efluid.fr>
2018-12-05 13:46:34 +0100

    [fix] windows

--- src/start/Main.java
+++ src/start/Main.java
@@ -10,7 +10,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import resource.lang.Lang;
@@ -38,22 +37,23 @@ public class Main extends Application {
         prStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("../gui/NewUI2.fxml"));
         primaryStage.setTitle("IRC");
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
 
-        prStage.getIcons().add(new Image("/resource/Images/ediome2.png"));
+        prStage.getIcons().add(new Image("/resource/Images/logov3.png"));
 
         primaryStage.setScene(new Scene(root, 1280, 720));
         Main.getPrimaryStage().getScene().getStylesheets().add(getClass().getResource("..//gui/css/main-black.css").toExternalForm());
-
+        primaryStage.setResizable(true);
         primaryStage.show();
 
-        String OS = System.getProperty("os.name").toLowerCase();
+        String os = System.getProperty("os.name").toLowerCase();
 
-        if(OS.indexOf("win") >= 0) {
+        if(os.contains("win")) {
             long lhwnd = com.sun.glass.ui.Window.getWindows().get(0).getNativeWindow();
             Pointer lpVoid = new Pointer(lhwnd);
             WinDef.HWND hwnd = new WinDef.HWND(lpVoid);
             final User32 user32 = User32.INSTANCE;
+
             int oldStyle = user32.GetWindowLong(hwnd, WinUser.GWL_STYLE);
             System.out.println(Integer.toBinaryString(oldStyle));
             int newStyle = oldStyle | 0x00020000;//WS_MINIMIZEBOX
