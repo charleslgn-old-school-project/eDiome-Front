debbbbe3364abedbb4cd01b206da0da82d931d93
CharlesLgn <charles.ligony3@etu.univ-lorraine.fr>
2019-01-04 18:12:11 +0100

    [fix] stack don't work

--- src/main/java/com/ircfront/controller/CreationController.java
+++ src/main/java/com/ircfront/controller/CreationController.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -28,7 +29,7 @@ public class CreationController implements Initializable {
   @FXML
   private Label lblEror;
   @FXML
-  private StackPane panParent;
+  private GridPane panParent;
   @FXML
   private TextField prenom;
   @FXML
