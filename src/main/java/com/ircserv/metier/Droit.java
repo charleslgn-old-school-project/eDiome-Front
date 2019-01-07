8a0cd3381b927830c2e2f1c4aa81ea71cf3abb22
Lucas <lucas.cuoco@gmail.com>
2019-01-07 11:10:03 +0100

    [feat] Hibernate : - mise en place des Droits

---
+++ src/main/java/com/ircserv/metier/Droit.java
@@ -0,0 +1,41 @@
+package com.ircserv.metier;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+
+public class Droit implements Serializable {
+
+    int id_droit;
+    String libelle;
+    String description;
+
+
+    public Droit() {
+    }
+
+    public int getId_droit() {
+        return id_droit;
+    }
+
+    public void setId_droit(int id_droit) {
+        this.id_droit = id_droit;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
